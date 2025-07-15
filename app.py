b9fd40c2fb2d4f567930f1ab54c7580b4674b136
subodh kumar yadav <subodhkumary933@gmail.com>
2025-07-15 07:27:33 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, render_template, request
 from recommender import recommend_restaurant
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route('/')
 def index():
@@ -16,6 +17,5 @@ def recommend():
     return render_template('result.html', name=name, recommendations=recommendations, error=None)
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
